4b9f5a90b6ac02177de913d15b67a1a20c539cad
Jackson Coxson <jkcoxson@gmail.com>
2024-07-30 14:23:26 -0700

    Change the competition to a 0.8 multiplier

--- competition.py
+++ competition.py
@@ -13,7 +13,12 @@ import dashboard
 def handle_request(request: holly.ParsedHollyMessage):
     """Parses the text to determine if we need to react"""
     print(request)
-    if request.is_targeted and (request.match('what score') or request.match('whats score') or request.match('who winning') or request.match('get score')):
+    if request.is_targeted and (
+        request.match("what score")
+        or request.match("whats score")
+        or request.match("who winning")
+        or request.match("get score")
+    ):
         # if request.chat_id != "7016741568410945":
         #     return "shhhhhhh it's a secret"
         return get_score()
@@ -25,7 +30,7 @@ def get_score():
     """Gets the score of contacted/total referrals"""
     try:
         client = chirch.ChurchClient()
-        persons = client.get_cached_people_list()['persons']
+        persons = client.get_cached_people_list()["persons"]
 
         now = datetime.datetime.now()
         # get the last Sunday
@@ -41,8 +46,7 @@ def get_score():
             assigned_date = person.get("referralAssignedDate")
             if assigned_date is None:
                 continue
-            assigned_date = datetime.datetime.fromtimestamp(
-                assigned_date / 1000)
+            assigned_date = datetime.datetime.fromtimestamp(assigned_date / 1000)
             if assigned_date < last_sunday:
                 continue
             try:
@@ -63,7 +67,7 @@ def get_score():
                     dashboard.PersonStatus.NOT_RECENTLY_CONTACTED,
                     dashboard.PersonStatus.PROGRESSING_GREEN,
                     dashboard.PersonStatus.TOO_BUSY,
-                    dashboard.PersonStatus.YELLOW
+                    dashboard.PersonStatus.YELLOW,
                 ]
 
                 if dot_status not in permissable_dots:
@@ -79,18 +83,23 @@ def get_score():
                 status = dashboard.ReferralStatus(status_id)
 
                 if status == dashboard.ReferralStatus.NOT_ATTEMPTED:
-                    zones[zone] = (zones[zone][0], zones[zone]
-                                   [1], zones[zone][2] + 1)
+                    zones[zone] = (zones[zone][0], zones[zone][1], zones[zone][2] + 1)
                     print(f"{zone.name}: {person.get('lastName')}")
                     total_referrals += 1
                 elif status == dashboard.ReferralStatus.NOT_SUCCESSFUL:
-                    zones[zone] = (zones[zone][0], zones[zone]
-                                   [1] + 1, zones[zone][2] + 1)
+                    zones[zone] = (
+                        zones[zone][0],
+                        zones[zone][1] + 1,
+                        zones[zone][2] + 1,
+                    )
                     total_referrals += 1
                     total_attempted += 1
                 elif status == dashboard.ReferralStatus.SUCCESSFUL:
-                    zones[zone] = (zones[zone][0] + 1, zones[zone]
-                                   [1], zones[zone][2] + 1)
+                    zones[zone] = (
+                        zones[zone][0] + 1,
+                        zones[zone][1],
+                        zones[zone][2] + 1,
+                    )
                     total_referrals += 1
                     total_attempted += 1
                     total_successful += 1
@@ -102,13 +111,14 @@ def get_score():
         for zone, zone_items in zones.items():
             if zone_items[2] != 0:
                 print(
-                    f"{zone.name} - S:{zone_items[0]} A:{zone_items[1]} T:{zone_items[2]}")
+                    f"{zone.name} - S:{zone_items[0]} A:{zone_items[1]} T:{zone_items[2]}"
+                )
                 zone_percentages[zone] = (
-                    (zone_items[0] + (zone_items[1] * 0.3)) / zone_items[2]) * 1000  # change me for weighted successful
+                    (zone_items[0] + (zone_items[1] * 0.8)) / zone_items[2]
+                ) * 1000  # change me for weighted successful
 
         # Rank the zones
-        ranked = sorted(zone_percentages.items(),
-                        key=lambda x: x[1], reverse=True)
+        ranked = sorted(zone_percentages.items(), key=lambda x: x[1], reverse=True)
 
         # Create the string
         res = "the current winners of my dog bowl. are the following hooomans:\n\n"
@@ -126,13 +136,17 @@ def get_score():
         res += "\n"
         for zone, percentage in ranked:
             percent_str = round(percentage)
-            zone_name = zone.name.replace('_', ' ').capitalize()
+            zone_name = zone.name.replace("_", " ").capitalize()
             res += f"{zone_name}: {percent_str}\n"
 
         res += f"\nthis transfer we have received {total_referrals} referrals. of which {total_attempted} were attempted. and {total_successful} were successful! so many new people to meet!\n"
 
-        funny = ["my tail might wag off", "I hope they like fetch",
-                 "I want to jump on them all", "i will be friends with all of them"]
+        funny = [
+            "my tail might wag off",
+            "I hope they like fetch",
+            "I want to jump on them all",
+            "i will be friends with all of them",
+        ]
         res += random.choice(funny)
 
         return res
@@ -148,19 +162,22 @@ def main():
     while True:
         try:
             client = holly.HollyClient()
-            print('Connected to Holly')
+            print("Connected to Holly")
             while True:
                 raw_msg = client.recv()
                 print(raw_msg)
                 ret = handle_request(raw_msg.parse(parser))
                 if ret:
-                    client.send(holly.HollyMessage(
-                        content=ret, chat_id=raw_msg.chat_id, sender=''))
+                    client.send(
+                        holly.HollyMessage(
+                            content=ret, chat_id=raw_msg.chat_id, sender=""
+                        )
+                    )
 
         except holly.HollyError as e:
             print(f"Error: {e}")
 
-        print('Disconnected from Holly socket')
+        print("Disconnected from Holly socket")
         time.sleep(30)
 
 
