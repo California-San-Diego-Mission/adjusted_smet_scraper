66144a0692f678f11760ffd6b15268ea115bfdfe
Jackson Coxson <jkcoxson@gmail.com>
2024-08-02 15:34:33 -0700

    Add the average contact time to the calculation of the score

--- competition.py
+++ competition.py
@@ -4,10 +4,17 @@
 import datetime
 import time
 import random
+import mysql.connector
+import os
+import pytz
+import statistics
+from dotenv import load_dotenv
 
 import holly
 import chirch
 import dashboard
+
+load_dotenv()
 
 
 def handle_request(request: holly.ParsedHollyMessage):
@@ -29,14 +36,21 @@ def handle_request(request: holly.ParsedHollyMessage):
 def get_score():
     """Gets the score of contacted/total referrals"""
     try:
+
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=os.getenv("MYSQL_USERNAME"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database="holly",
+        )
+        cursor = mydb.cursor()
+
         client = chirch.ChurchClient()
         persons = client.get_cached_people_list()["persons"]
 
         now = datetime.datetime.now()
-        # get the last Sunday
-        # last_sunday = now - datetime.timedelta(days=now.weekday(), weeks=1)
-        # last_sunday = last_sunday.replace(hour=0, minute=0, second=0, microsecond=0)
-        last_sunday = datetime.datetime.fromtimestamp(1720033200)
+        # get the last transfer
+        last_transfer = datetime.datetime.fromtimestamp(1720033200)
 
         zones = {}
         total_referrals = 0
@@ -47,7 +61,7 @@ def get_score():
             if assigned_date is None:
                 continue
             assigned_date = datetime.datetime.fromtimestamp(assigned_date / 1000)
-            if assigned_date < last_sunday:
+            if assigned_date < last_transfer:
                 continue
             try:
                 zone_id = person.get("zoneId")
@@ -75,15 +89,32 @@ def get_score():
 
                 # If the zone doesn't exist, insert it
                 if zones.get(zone) is None:
-                    zones[zone] = (0, 0, 0)
+                    zones[zone] = (0, 0, 0, [])
 
                 status_id = person.get("referralStatusId")
                 if status_id is None:
                     continue
                 status = dashboard.ReferralStatus(status_id)
 
+                # Get the time from assignment to contact
+                if (
+                    status == dashboard.ReferralStatus.NOT_SUCCESSFUL
+                    or status == dashboard.ReferralStatus.SUCCESSFUL
+                ):
+                    contact_time = get_contact_time(
+                        person.get("personGuid"), cursor, client
+                    )
+                    if contact_time is not None:
+                        mydb.commit()
+                        zones[zone][3].append(contact_time)
+
                 if status == dashboard.ReferralStatus.NOT_ATTEMPTED:
-                    zones[zone] = (zones[zone][0], zones[zone][1], zones[zone][2] + 1)
+                    zones[zone] = (
+                        zones[zone][0],
+                        zones[zone][1],
+                        zones[zone][2] + 1,
+                        zones[zone][3],
+                    )
                     print(f"{zone.name}: {person.get('lastName')}")
                     total_referrals += 1
                 elif status == dashboard.ReferralStatus.NOT_SUCCESSFUL:
@@ -91,6 +122,7 @@ def get_score():
                         zones[zone][0],
                         zones[zone][1] + 1,
                         zones[zone][2] + 1,
+                        zones[zone][3],
                     )
                     total_referrals += 1
                     total_attempted += 1
@@ -99,6 +131,7 @@ def get_score():
                         zones[zone][0] + 1,
                         zones[zone][1],
                         zones[zone][2] + 1,
+                        zones[zone][3],
                     )
                     total_referrals += 1
                     total_attempted += 1
@@ -109,13 +142,14 @@ def get_score():
 
         zone_percentages = {}
         for zone, zone_items in zones.items():
+            time_average = statistics.mean(zone_items[3])
             if zone_items[2] != 0:
                 print(
-                    f"{zone.name} - S:{zone_items[0]} A:{zone_items[1]} T:{zone_items[2]}"
+                    f"{zone.name} - S:{zone_items[0]} A:{zone_items[1]} T:{zone_items[2]} M:{time_average}"
                 )
                 zone_percentages[zone] = (
-                    (zone_items[0] + (zone_items[1] * 0.8)) / zone_items[2]
-                ) * 1000  # change me for weighted successful
+                    ((zone_items[0] + (zone_items[1] * 0.8)) / zone_items[2]) * 1000
+                ) - time_average  # change me for weighted successful
 
         # Rank the zones
         ranked = sorted(zone_percentages.items(), key=lambda x: x[1], reverse=True)
@@ -153,6 +187,63 @@ def get_score():
     except Exception as e:
         print(f"Error getting score: {e}")
         return "*bark* unable to fetch the score *bark*"
+
+
+def get_contact_time(guid: str, cursor, church_client) -> int:
+    cursor.execute("SELECT contact_time FROM people WHERE guid = %s", (guid,))
+    res = cursor.fetchone()
+    if res is None:
+        print("Getting the contact time for ", guid)
+        timeline = church_client.get_person_timeline(guid)
+        referral_time = 0
+        contact_time = 0
+        for idx, event in enumerate(timeline):
+            if event["timelineItemType"] == "NEW_REFERRAL":
+                referral_time = int(event["itemDate"] / 1000)
+                break
+            elif event["timelineItemType"] == "CONTACT":
+                contact_time = int(event["itemDate"] / 1000)
+        if referral_time == 0 or contact_time == 0:
+            return None
+        referral_time = adjust_epoch_time(referral_time)
+        contact_time = adjust_epoch_time(contact_time)
+        delta = contact_time - referral_time
+
+        minutes_difference = delta.total_seconds() / 60
+        if minutes_difference < 0:
+            print("Delta was calculated incorrectly!")
+            print("Referral time: ", referral_time)
+            print("Contact time: ", contact_time)
+            print("Delta: ", minutes_difference)
+
+        cursor.execute(
+            "INSERT INTO people (guid, contact_time) VALUES (%s, %s)",
+            (guid, minutes_difference),
+        )
+        return minutes_difference
+    else:
+        return res[0]
+
+
+def adjust_epoch_time(epoch_time):
+    # Convert the epoch time to a datetime object
+    timezone = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+    dt = datetime.datetime.fromtimestamp(epoch_time, timezone)
+
+    # Define the start and end times
+    start_time = dt.replace(hour=6, minute=30, second=0, microsecond=0)
+    end_time = dt.replace(hour=22, minute=30, second=0, microsecond=0)
+
+    if start_time <= dt <= end_time:
+        # Time is within the range
+        return dt
+    else:
+        # Time is outside the range, set to 6:30 AM of the next day
+        next_day = dt
+        if dt.hour > 10:  # doesn't have to be 10, just a number that checks if AM/PM
+            next_day = dt + datetime.timedelta(days=1)
+        adjusted_time = next_day.replace(hour=6, minute=30, second=0, microsecond=0)
+        return adjusted_time
 
 
 def main():
---
+++ up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE people (
+    guid VARCHAR(255),
+    contact_time INT,
+    PRIMARY KEY (guid)
+);
