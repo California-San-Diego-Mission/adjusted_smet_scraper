1c0a1168a8e66a4a2739cb56eddfbdb7d438bfc8
advojote <joseph.h.davis@missionary.org>
2025-05-13 15:27:55 -0700

    made security updates based on chatgpt feedback

--- sql_library.py
+++ sql_library.py
@@ -4,11 +4,6 @@ import os
 from datetime import date
 import re
 
-def is_valid_date_format(date_string):
-    pattern = r'^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$'
-    return bool(re.match(pattern, date_string))
-
-
 dotenv.load_dotenv()
 
 mydb = mysql.connector.connect(
@@ -19,6 +14,10 @@ mydb = mysql.connector.connect(
 )
 
 cursor = mydb.cursor()
+
+def is_valid_date_format(date_string):
+    pattern = r'^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$'
+    return bool(re.match(pattern, date_string))
 
 def zone_is_allowed(zone):
     allowed_zones = [i for i in range(1, 9)]  # Example allowed zones: z1 to z8
@@ -38,15 +37,31 @@ def count_zone_blank_slates(zone):
 def mark_today_zone_blank_slate(zone):
     mark_zone_blank_slate_on_day(zone, date.today().strftime("%Y-%m-%d"))
 
-def mark_zone_blank_slate_on_day(zone, day):
-    zone_is_allowed(zone)
-    zone_column = create_column_from_zone(zone)
+def valid_date(day):
     if not is_valid_date_format(day):
         raise ValueError("Invalid date")
-    query = f"INSERT INTO zone_report_history (day, {zone_column}) VALUES ('{day}', 1) ON DUPLICATE KEY UPDATE {zone_column} = 1;"
-    cursor.execute(query)
+
+def mark_zone_blank_slate_on_day(zone, day):
+    #expects an integer zone number and a string yyyy-mm-dd date
+    zone_is_allowed(zone)
+    zone_column = create_column_from_zone(zone)
+    valid_date(day)
+    query = f"""
+        INSERT INTO zone_report_history (day, {zone_column})
+        VALUES (%s, %s)
+        ON DUPLICATE KEY UPDATE {zone_column} = %s;
+        """
+    cursor.execute(query, (day, 1, 1))
     mydb.commit()
+
+def count_blank_slates_in_zone_since_day(zone, day):
+    #expects an integer zone number and a string yyyy-mm-dd date
+    zone_is_allowed()
+    zone_column = create_column_from_zone(zone)
+    valid_date(day)
+    query = f"select count(*) from zone_report_history where {zone_column} = 1 AND day >= %s;"
+    cursor.execute(query, (day))
     
 
-# mark_zone_blank_slate_on_day(4, "2023-07-31")
-mark_today_zone_blank_slate(1)
+mark_zone_blank_slate_on_day(5, "2023-07-31")
+# mark_today_zone_blank_slate(1)
