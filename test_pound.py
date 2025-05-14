c5785a361cf5906eb62e07681fdb0f43fa7e9d16
advojote <joseph.h.davis@missionary.org>
2025-05-14 15:45:43 -0700

    all blank_slate counting and reporting functionality seems to work!!!!

--- test_pound.py
+++ test_pound.py
@@ -16,8 +16,9 @@ import competition
 import dashboard
 from person import Person
 import pound_statics
+import sql_library
 
-risky_global_report_status = 0
+# risky_global_report_status = 0
 #This is a bad idea implemented by Elder Davis to make keeping track of empty zone reports easy. When the generate_report function is run,
 # it will set this variable to 0 every single time. If the report is empty, it will set this value to 1, and then the main function will check
 # this variable to determine whether or not to record an instance of an empty report day
@@ -40,8 +41,8 @@ def load_today_report() -> Union[dict[str, dict[str, list[str]]], None]:
 
 def generate_report(requested_zone: dashboard.Zone) -> Optional[str]:
     """Generates a report of uncontacted referrals"""
-    global risky_global_report_status
-    risky_global_report_status= 0
+    # global risky_global_report_status
+    # risky_global_report_status= 0
     zones = load_today_report()
 
     if zones is None:
@@ -79,11 +80,12 @@ def generate_report(requested_zone: dashboard.Zone) -> Optional[str]:
         )
         zones = json.loads(json.dumps(zones))  # lazy I know
 
-    zone = zones.get(str(requested_zone.value))
+    zone = zones.get(str(requested_zone.name))
     if zone is None:
-        print(zone)
-        risky_global_report_status = 1
-        return 'NO UNCONTACTED REFERRALS!!11! Your zone earned a bonus for a blank slate!'
+        zone_number = competition.strip_zone_number_from_name(requested_zone.name)
+        print(f'Zone {zone_number} got a blank slate today')
+        sql_library.mark_today_zone_blank_slate(zone_number)
+        return f'NO UNCONTACTED REFERRALS!!11! Your zone earned a bonus for a blank slate!\n{choice(pound_statics.blank_slates)}'
     if zone:
         message = ''
         for area, names in zone.items():
@@ -100,16 +102,15 @@ def main():
     chirch.ChurchClient().login()
     holly_client = holly.HollyClient()
 
-    # for zone in dashboard.Zone:
-    #     generate_report(zone)
-    #     if risky_global_report_status != 0:
+    for zone in dashboard.Zone:
+        generate_report(zone)
             
-
     score = competition.get_score()
 
     # Iterate over all the zones
     for zone in dashboard.Zone:
-        print(zone)
+        # print(competition.strip_zone_number_from_name(zone.name))
+        # print("that was the zone")
         res_message = f'{choice(pound_statics.morning)}\n\n'
         res_message += choice(pound_statics.score_intro) + '\n'
         res_message += score
